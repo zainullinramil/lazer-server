4aaae76c55f20118fa0c0aec0e197de2afdfd4db
Ramil <zr@imagespark.ru>
2018-12-25 17:31:17 +0300

    socket emit

--- index.js
+++ index.js
@@ -4,15 +4,26 @@ const WebSocket = require("ws");
 
 const server = new WebSocket.Server({ port: 3003 });
 
-const port = new SerialPort("COM2", {
-  baudRate: 28800
-});
+// const port = new SerialPort("/dev/cu.Bluetooth-Incoming-Port", {
+//   baudRate: 28800
+// });
 
-const parser = port.pipe(new Delimiter({ delimiter: "\n" }));
+// const parser = port.pipe(new Delimiter({ delimiter: "\n" }));
 
 server.on("connection", ws => {
-  ws.send("you connected lazer server");
-  parser.on("data", data => (console.log(data), ws.send(data))); // emits data after every '\n'
+    ws.on('message', message => {
+        let parseMessage;
+        try {
+            parseMessage = JSON.parse(message);
+        } catch (e) {
+            console.log('catch', e)
+        }
+        const { command } = parseMessage;
+        if (command && command === 'up') {
+            ws.send('{"data": "' + Date.now() + '"}');
+        }
+    });
+//   parser.on("data", data => (console.log(data), ws.send(data))); // emits data after every '\n'
 });
 
 // port.open(function(err) {
