25627c96f9c874ab5196252979a1a07474b087b8
Рамиль ЗайнуллинÐ <zr@imagespark.ru>
2018-12-25 22:29:48 +0300

    data parse

--- index.js
+++ index.js
@@ -1,29 +1,69 @@
 const SerialPort = require("serialport");
 const Delimiter = require("@serialport/parser-delimiter");
+const ByteLength = require("@serialport/parser-byte-length");
+const Readline = require("@serialport/parser-readline");
+const Ready = require("@serialport/parser-ready");
+const CCTalk = require("@serialport/parser-cctalk");
 const WebSocket = require("ws");
 
 const server = new WebSocket.Server({ port: 3003 });
 
-// const port = new SerialPort("/dev/cu.Bluetooth-Incoming-Port", {
-//   baudRate: 28800
-// });
+const port = new SerialPort("COM2");
 
+const parser = port.pipe(new ByteLength({ length: 1023 }));
 // const parser = port.pipe(new Delimiter({ delimiter: "\n" }));
+// const parser = port.pipe(new Readline({ delimiter: "\r\n" }));
+// const parser = port.pipe(new Ready({ delimiter: "READY" }));
+// const parser = port.pipe(new CCTalk());
+let comData = "";
+let webSocket = null;
+
+// parser.on("data", data => {
+//   comData = data;
+// });
+const sendData = data => {
+  if (webSocket && webSocket.readyState === 1) {
+    webSocket.send(JSON.stringify(data));
+  }
+};
 
 server.on("connection", ws => {
-    ws.on('message', message => {
-        let parseMessage;
-        try {
-            parseMessage = JSON.parse(message);
-        } catch (e) {
-            console.log('catch', e)
-        }
-        const { command } = parseMessage;
-        if (command && command === 'up') {
-            ws.send('{"data": "' + Date.now() + '"}');
-        }
-    });
-//   parser.on("data", data => (console.log(data), ws.send(data))); // emits data after every '\n'
+  //   port.write(Buffer.from("001 005 110"), function(err) {
+  //     if (err) {
+  //       return console.log("Error on write: ", err.message);
+  //     }
+  //     console.log("message written");
+  //   });
+
+  webSocket = ws;
+  ws.on("message", message => {
+    let parseMessage;
+
+    try {
+      parseMessage = JSON.parse(message);
+    } catch (e) {
+      console.log("catch", e);
+    }
+
+    const { command } = parseMessage;
+    if (command && command === "send") {
+      port.write("Hi Mom!");
+    }
+  });
+  parser.on("data", data => {
+    // sendData(data);
+    if (ws && ws.readyState === 1) {
+      ws.send(JSON.stringify(data));
+    }
+  });
+  ws.on("close", (code, reason) => {
+    webSocket = null;
+    ws.terminate();
+  });
+});
+
+server.on("error", error => {
+  console.log("error", error);
 });
 
 // port.open(function(err) {
